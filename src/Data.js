13f3b33e7148b81d45783f51e6142532484d03de
isabelchua <isabelchua86@gmail.com>
2020-11-25 13:34:33 -0800

    load name and image

--- src/Data.js
+++ src/Data.js
@@ -1,26 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const fetchRandomData = () => {
 	return axios
 		.get("https://randomuser.me/api")
 		.then(({ data }) => {
 			console.log(data);
-			return JSON.stringify(data, null, 2);
+			return data;
+			//return JSON.stringify(data, null, 2);
 		})
 		.catch(err => {
 			console.error(err);
 		});
 };
 
+const getFullUserName = userInfo => {
+	const {
+		name: { first, last }
+	} = userInfo;
+	return `${first} ${last}`;
+};
+
 const Data = () => {
 	const [randomUserDataJSON, setRandomUserDataJSON] = useState("");
+	const [userInfos, setUserInfos] = useState([]);
 
 	useEffect(() => {
 		fetchRandomData().then(randomData => {
-			setRandomUserDataJSON(randomData || "No user data found.");
+			setRandomUserDataJSON(
+				JSON.stringify(randomData, null, 2) || "No user data found.",
+				setUserInfos(randomData.results)
+			);
 		});
 	}, []);
-	return <div>{randomUserDataJSON}</div>;
+	return (
+		<div>
+			{userInfos.map((userInfo, idx) => (
+				<div key={idx}>
+					<p>{getFullUserName(userInfo)}</p>
+					<img src={userInfo.picture.thumbnail} alt="pic" />
+				</div>
+			))}
+			<pre>{randomUserDataJSON}</pre>
+		</div>
+	);
 };
 
 export default Data;
