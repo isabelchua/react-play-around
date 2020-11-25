cdde4d6f3ca3fdcfeebb28967245c198280dd54c
isabelchua <isabelchua86@gmail.com>
2020-11-25 13:49:59 -0800

    fetching more users

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const fetchRandomData = () => {
+const fetchRandomData = pageNumber => {
 	return axios
-		.get("https://randomuser.me/api")
+		.get(`https://randomuser.me/api?page=${pageNumber}`)
 		.then(({ data }) => {
 			console.log(data);
 			return data;
@@ -24,17 +24,37 @@ const getFullUserName = userInfo => {
 const Data = () => {
 	const [randomUserDataJSON, setRandomUserDataJSON] = useState("");
 	const [userInfos, setUserInfos] = useState([]);
+	const [nextPageNumber, setNextPageNumber] = useState(1);
+
+	const fetchNextUser = () => {
+		fetchRandomData().then(randomData => {
+			//setRandomUserDataJSON(JSON.stringify(randomData, null, 2) || "No user data found."
+			const newUserInfos = [...userInfos, ...randomData.results];
+			setUserInfos(newUserInfos);
+			setNextPageNumber(randomData.info.page + 1);
+		});
+		// fetchRandomData().then(randomData => {
+		// 	setRandomUserDataJSON(
+		// 		JSON.stringify(randomData, null, 2) || "No user data found.",
+		// 		setUserInfos(randomData.results),
+		// 		setNextPageNumber(randomData.info.page +1)
+		// 	);
+		// });
+	};
 
 	useEffect(() => {
-		fetchRandomData().then(randomData => {
-			setRandomUserDataJSON(
-				JSON.stringify(randomData, null, 2) || "No user data found.",
-				setUserInfos(randomData.results)
-			);
-		});
+		//fetchNextUser()
+		fetchNextUser();
 	}, []);
 	return (
 		<div>
+			<button
+				onClick={() => {
+					fetchNextUser();
+				}}
+			>
+				Fetch Another User
+			</button>
 			{userInfos.map((userInfo, idx) => (
 				<div key={idx}>
 					<p>{getFullUserName(userInfo)}</p>
